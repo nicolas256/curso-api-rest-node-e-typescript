443adab3ab0ea3eff6e83d7a42e624f4b30a8240
Nicolas de Andrade <nicolasnipe@gmail.com>
2024-01-18 09:15:43 -0300

    feat : new configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
-
 const server = express();
 
 
-server.get('/', (req, res) =>{
+interface Teste {
 
-    return res.send('Olá, Dev!');
-} );
+};
 
+server.get('/', (req, res) => { return res.send('Olá, Dev!'); });
 
 export { server };
